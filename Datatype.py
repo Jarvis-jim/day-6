802f365d2814d99ec7c4c1004b5494e3ef6b8342
jim <jjakaria401@gmail.com>
2023-08-07 21:12:03 +0600

    python

---
+++ Datatype.py
@@ -0,0 +1,44 @@
+# int type data 
+
+hablu = 420
+
+print(type(hablu))
+
+
+#floating type data 
+gublu = 40.2
+print(type(gublu))
+# complex type data
+kodu = 420j
+print(type(kodu))
+
+
+#str type data
+yourName = 'hablu'
+myName = 'jim'
+print("my name is" + ' '+myName)
+
+#bool type data 
+bool = True
+print(type(bool))
+
+x = 8
+y = 10
+
+print(x == y)
+
+#Binary type data
+habluList = [1,2,3,54,41,99,]
+b =  bytes(habluList)
+print(type(b))
+
+#Binary type data bytesArray
+habluList = [1,2,3,54,69,84,98,]
+b1 = bytearray(habluList)
+b1[1] = 100
+print(b1[1])
+
+#None type data
+x = None
+print(type(x))
+
---
+++ format.py
@@ -0,0 +1,5 @@
+num1 = 20
+num2 = 10
+username = "jim"
+Roll = "10"
+print(f"my name is {username} & my class roll is {Roll}")
